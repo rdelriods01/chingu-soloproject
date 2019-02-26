2603069e760e6ce3b2bf04e3453d38b9cebc408e
Ricardo Del Rio <rdelrio.ds@gmail.com>
2019-02-26 15:22:43 -0600

    move apicall to parent Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
+import firebase from 'firebase/app';
+import fbconfig from './fbconfig';
+import 'firebase/functions';
+
 import Searchbar from './Components/Searchbar';
+import Gallery from './Components/Gallery';
+
+const fb = firebase.initializeApp(fbconfig);
 
 class App extends Component {
+
+  state = {
+    items: []
+  }
+
+  searchBook = (query) => {
+    const readBooks = fb.functions().httpsCallable('searchBook');
+    readBooks(query).then(res => {
+      console.log(res);
+      this.setState({
+        items: res.data.items
+      })
+    })
+  }
   render() {
     return (
       <div className="App">
         <div className="upper">
           <h1>BOOK FINDER</h1>
-          <Searchbar />
+          <Searchbar search={(query) => this.searchBook(query)} />
         </div>
         <div className="lower">
-
+          <Gallery items={this.state.items} />
         </div>
       </div>
     );
--- src/Components/Searchbar.jsx
+++ src/Components/Searchbar.jsx
@@ -1,15 +1,9 @@
 import React, { Component } from 'react'
-import firebase from 'firebase/app';
-import fbconfig from '../fbconfig';
-import 'firebase/functions';
-
-const fb = firebase.initializeApp(fbconfig);
-
 
 export class Searchbar extends Component {
 
     state = {
-        searchInput: ''
+        searchInput: '',
     }
 
     render() {
@@ -35,10 +29,7 @@ export class Searchbar extends Component {
             alert('Please provide a title or author in the input search')
         } else {
             console.log('It will search for: ' + this.state.searchInput);
-            const readBooks = fb.functions().httpsCallable('searchBook');
-            readBooks(this.state.searchInput).then(res => {
-                console.log(res);
-            })
+            this.props.search(this.state.searchInput);
         }
     }
 }

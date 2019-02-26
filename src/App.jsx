03e80de448e085b06b84a759f777f2a2313b4db4
Ricardo Del Rio <rdelrio.ds@gmail.com>
2019-02-25 19:00:11 -0600

    change classname superior to upper

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="superior">
+        <div className="upper">
           <h1>BOOK FINDER</h1>
           <Searchbar />
+        </div>
+        <div className="lower">
+
         </div>
       </div>
     );

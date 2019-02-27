0d0febfcd33e05925794fc0616223c368d0da2b1
Ricardo Del Rio <rdelrio.ds@gmail.com>
2019-02-27 12:02:28 -0600

    separate loader styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import './loader.scss';
 import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
 

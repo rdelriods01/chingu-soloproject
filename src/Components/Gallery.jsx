a060829b92582b6ba7fa809d5f102158884fba49
Ricardo Del Rio <rdelrio.ds@gmail.com>
2019-02-26 21:19:51 -0600

    fix bugs with gallery (mediaquery/conditions in props)

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -13,8 +13,8 @@ export default class Gallery extends Component {
                                     <img src={imageLinks !== undefined ? imageLinks.thumbnail : ''} alt="book" className="bookImage" />
                                     <div className="details">
                                         <h4>{title}</h4>
-                                        <p>By: {authors[0]} {(authors.length > 1) ? 'and others' : ''} </p>
-                                        <p>Published by: {publisher} </p>
+                                        {authors ? <p>By: {authors[0]} {(authors.length > 1) ? 'and others' : ''} </p> : <div></div>}
+                                        {publisher ? <p>Published by: {publisher} </p> : <div></div>}
                                         <div className="btn"><a href={infoLink}>See this book</a></div>
                                     </div>
                                 </div>

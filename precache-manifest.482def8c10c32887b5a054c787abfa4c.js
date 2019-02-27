94ef90e596e8c4c92a05c8184b2f30ddf7d9a815
Ricardo Del Rio <rdelrio.ds@gmail.com>
2019-02-27 14:35:44 -0600

    Updates

---
+++ precache-manifest.482def8c10c32887b5a054c787abfa4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "73b13e4e2ddd304bdd0b",
+    "url": "/chingu-soloproject/static/js/runtime~main.73b13e4e.js"
+  },
+  {
+    "revision": "4242dd7376011ba2d9ed",
+    "url": "/chingu-soloproject/static/js/main.4242dd73.chunk.js"
+  },
+  {
+    "revision": "ed9ecd26a765b16ea57b",
+    "url": "/chingu-soloproject/static/js/2.ed9ecd26.chunk.js"
+  },
+  {
+    "revision": "4242dd7376011ba2d9ed",
+    "url": "/chingu-soloproject/static/css/main.133d1fd6.chunk.css"
+  },
+  {
+    "revision": "efe799305c50448181e5222896bd1250",
+    "url": "/chingu-soloproject/index.html"
+  }
+];

444bbaa7014d9ce5706e8a93530a440398d4c790
Ricardo Del Rio <rdelrio.ds@gmail.com>
2019-02-26 20:55:33 -0600

    style to gallery

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -3,19 +3,27 @@ import React, { Component } from 'react';
 export default class Gallery extends Component {
     render() {
         return (
-            <div>
+            <div className="gallery">
                 {
-                    this.props.items.map((item, i) => {
-                        let { title, imageLinks, infoLink } = item.volumeInfo
+                    this.props.items && this.props.items.map((item, i) => {
+                        let { title, imageLinks, infoLink, authors, publisher } = item.volumeInfo
                         return (
-                            <a href={infoLink} key={i} className="book">
-                                <img src={imageLinks !== undefined ? imageLinks.thumbnail : ''} alt="book" className="bookImage" />
-                                <div className="titleText">{title}</div>
-                            </a>
+                            <div className="card" key={i}>
+                                <div className="grid">
+                                    <img src={imageLinks !== undefined ? imageLinks.thumbnail : ''} alt="book" className="bookImage" />
+                                    <div className="details">
+                                        <h4>{title}</h4>
+                                        <p>By: {authors[0]} {(authors.length > 1) ? 'and others' : ''} </p>
+                                        <p>Published by: {publisher} </p>
+                                        <div className="btn"><a href={infoLink}>See this book</a></div>
+                                    </div>
+                                </div>
+
+                            </div>
                         );
                     })
-                }</div>
-
+                }
+            </div>
         );
     }
 }
